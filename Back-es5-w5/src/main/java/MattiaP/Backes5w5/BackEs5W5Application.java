9a07e0da68fc3b3606672d47c590d76879b3edea
Tia-11 <143324169+Tia-11@users.noreply.github.com>
2024-01-12 17:09:03 +0100

    1

---
+++ Back-es5-w5/src/main/java/MattiaP/Backes5w5/BackEs5W5Application.java
@@ -0,0 +1,13 @@
+package MattiaP.Backes5w5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEs5W5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEs5W5Application.class, args);
+	}
+
+}
---
+++ Back-es5-w5/src/test/java/MattiaP/Backes5w5/BackEs5W5ApplicationTests.java
@@ -0,0 +1,13 @@
+package MattiaP.Backes5w5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEs5W5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
